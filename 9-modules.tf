edc1fcdd804aef71d9f9e995dc545cb36ae3fa01
Serhiy Slipchuk <slipchuks@ukr.net>
2024-07-28 01:49:24 +0300

    Initial commit

---
+++ 9-modules.tf
@@ -0,0 +1,39 @@
+
+# ================================================================================
+# optional modules
+# ================================================================================
+
+
+# nginx ingress controller
+# ================================================================================
+module "ingress-nginx" {
+  source = "./modules/ingress-nginx/"
+
+  depends_on = [ 
+    google_container_node_pool.general
+   ]
+}
+
+# cert manager (letsencrypt)
+# ================================================================================
+module "certmanager" {
+  source = "./modules/cert-manager/"
+
+  depends_on = [ 
+    google_container_node_pool.general
+   ]
+}
+
+# simple nginx container with an ingress
+# (point your hostname / dns to the public ip of gke cluster 'ingress-loadbalancer-ip')
+# ================================================================================
+module "test_app" {
+  source   = "./modules/testapp/"
+  hostname = "gke-nginx.slipchuk.website"
+
+  depends_on = [
+    module.ingress-nginx,
+    module.certmanager
+  ]
+}
+
